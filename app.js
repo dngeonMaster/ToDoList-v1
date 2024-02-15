295a94e3f95c175d78d9ecd0ef554bcd64bdeac4
dngeonMasteer <kaushikborah4080@gmail.com>
2024-02-16 01:43:16 +0530

    ejs

--- app.js
+++ app.js
@@ -5,18 +5,24 @@ const bodyParser=require("body-parser")
 
 const app= express(); 
 
-app.use("view engine","ejs");
+app.set("view engine","ejs");
 
 
 app.get("/",function(req,res){
+
      var today=new Date();
-     var currentDay=today.getDay();
-     if(currentDay===6 || currentDay===0){
-        res.sendFile(__dirname + "/index.html");     
-     }
-     else{
-        res.sendFile(__dirname + "/index.html")
-     }
+   
+    var options = {
+        weekday : "long",
+        day : "numeric",
+        month : "long",
+    };
+
+    var day=today.toLocaleDateString("en-US",options);
+
+     res.render("list",{
+        kindOfDay:day
+    });
 }); 
 
 
